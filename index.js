aabef4f27779ef0fdc72fa478ce65071af4b466c
yeluoqiuzhi <827884374@qq.com>
2018-04-17 10:31:13 +0800

    export main entry

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/react.production.min.js');
+  module.exports = require('./dist/gmodal.production.min');
 } else {
-  module.exports = require('./dist/react.development.js');
+  module.exports = require('./dist/gmodal.development');
 }
